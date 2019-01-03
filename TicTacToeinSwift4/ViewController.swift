928be335a385f704e7ce70da65ecda409a51a406
Chaman Gurjar <chamangujjar162@gmail.com>
2019-01-03 12:11:33 +0530

    Cross and circle shown based on user's click

--- TicTacToeinSwift4/ViewController.swift
+++ TicTacToeinSwift4/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 1 - is noughts and 2 - crosses
+    private var activePlayer = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    //    MARK: - Button's Action
     @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if activePlayer == 1 {
+            sender.setImage(UIImage(named: "nought"), for: .normal)
+            activePlayer = 2
+        } else {
+            sender.setImage(UIImage(named: "cross"), for: .normal)
+            activePlayer = 1
+        }
         
     }
     
